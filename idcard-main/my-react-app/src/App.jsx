a6cad6c24a082e6aa4dac40fa9c0465ddc6760cb
Nishantt0212 <nishant0212r@gmail.com>
2025-08-04 12:24:42 +0530

    Id- Card

---
+++ idcard-main/my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import IDCard from './components/IDCard'; // 👈 yahi import karna hai
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <IDCard /> {/* 👈 component use karo */}
+    </div>
+  );
+}
+
+export default App;
